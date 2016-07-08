103950e18c32c29282db0d0f01908f944935dbe3
Rohan Agrawal <rohankagrawal@gmail.com>
2016-07-07 23:34:43 -0700

    Add templates directory and gitignore file

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-from flask import Flask, g
+from flask import (Flask, g, render_template, flash, redirect, url_for)
+from flask.ext.login import LoginManager
+
+import forms
 import models
 
 DEBUG = True
@@ -32,7 +35,22 @@ def after_request(response):
 	g.db.close()
 	return response
 
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+	form = forms.RegisterForm()
+	if form.validate_on_submit():
+		flash("Yay, you registered!", "success")
+		models.User.create_user(
+			username=form.username.data,
+			email=form.email.data,
+			password=form.password.data
+		)
+		return redirect(url_for('index'))
+	return render_template('register.html', form=form)
 
+@app.route('/')
+def index():
+	return 'Hello'
 
 
 
